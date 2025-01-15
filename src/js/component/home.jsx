53863dd57b22c5f992cbfe6cdd863efb8ea70e57
Laurascardozo <143517864+Laurascardozo@users.noreply.github.com>
2025-01-15 02:39:24 +0000

    Todolist created

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,65 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState([]);
+	const [hoverItem, setHoverItem] = useState(null)
+
+	const handleChange = (event) => {
+		setInputValue(event.target.value);
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+		<div className="container">
+			<div id="title" className="rounded">
+				<h1>TODO LIST </h1>
+			</div>
+			<ul>
+				<div>
+					<input
+						className="todo-list"
+						type="text"
+						value={inputValue}
+						placeholder={todos.length === 0 ? "No things to do, add task" : "What needs to be done?"}
+						onChange={handleChange}
+						onKeyDown={(e) => {
+							if (e.key === 'Enter') {
+								setTodos(todos.concat(inputValue));
+								setInputValue("");
+							}
+						}
+						}>
+					</input>
+
+					{todos.map((item, index) => (
+						<div
+							key={index}
+							onMouseEnter={() => setHoverItem(index)}
+							onMouseLeave={() => setHoverItem(null)}
+						>
+
+							<li> {item}{""}{" "}
+								{hoverItem === index && <i className="fa-solid fa-trash p-20"
+									onClick={() =>
+										setTodos(todos.filter(
+											(todos, currentIndex) =>
+												index != currentIndex
+										))
+									}
+								></i>}
+							</li>
+
+						</div>
+					))}
+					<div className="task"> {todos.length} tasks left</div>
+				</div>
+
+			</ul>
+
 		</div>
+
+
 	);
 };
 
